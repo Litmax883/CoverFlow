ad8aa2b8183ad7468ca4297a3ffe09d2d0a375e7
Litmax <Litmax883@gmail.com>
2020-11-30 13:02:50 +0300

    Start init

---
+++ CoverFlow/CoverFlow/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoverFlow
+//
+//  Created by MAC on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
